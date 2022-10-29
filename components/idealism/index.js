3f42f67f9d85de5ec939230a2b3d4f7527cd4f5d
ericggul <57379903+ericggul@users.noreply.github.com>
2022-10-29 18:02:29 +0100

    Feat: Idealism components

---
+++ components/idealism/index.js
@@ -0,0 +1,44 @@
+import dynamic from "next/dynamic";
+
+//exporter
+import { useRef, useState, useEffect } from "react";
+import DownloadThreeScene from "utils/hoc/downloadThreeScene";
+import { extend, Canvas, useThree, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+const normalDistribution = (x) => Math.exp((-x * x) / 2) / Math.sqrt(2 * Math.PI);
+
+export default function Test() {
+  const VERTICAL_NUMBERS = 21;
+  const HORIZONTAL_NUMBERS = 51;
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [10, 10, 10] }}>
+        <ambientLight intensity={1} />
+
+        <group>
+          {new Array(VERTICAL_NUMBERS).fill(0).map((_, j) =>
+            new Array(HORIZONTAL_NUMBERS).fill(0).map((_, i) => {
+              const x = i - (HORIZONTAL_NUMBERS - 1) / 2;
+              const y = j - (VERTICAL_NUMBERS - 1) / 2;
+
+              const UNIT_SIZE = 0.75;
+              const MID_RELATIVE_SIZE = 0.6;
+              const SLOPE = 4;
+              const scale = Math.min(Math.abs(MID_RELATIVE_SIZE - Math.abs(y) / ((VERTICAL_NUMBERS + 1) / SLOPE)) + 0.05, 1);
+              console.log(scale);
+              return (
+                <mesh position={[x, 0, normalDistribution(x / 10) * 2.5 * y]} scale={[scale, scale, scale]} key={i}>
+                  <sphereGeometry attach="geometry" args={[UNIT_SIZE, 32, 32]} />
+                  <meshStandardMaterial attach="material" color="rgb(0,0,0)" />
+                </mesh>
+              );
+            })
+          )}
+        </group>
+        <OrbitControls />
+        {/* <DownloadThreeScene /> */}
+      </Canvas>
+    </div>
+  );
+}
--- pages/idealism/index.js
+++ pages/idealism/index.js
@@ -1,34 +1,7 @@
 import dynamic from "next/dynamic";
 
-//exporter
-import { useRef, useState, useEffect } from "react";
-import DownloadThreeScene from "utils/hoc/downloadThreeScene";
-import { extend, Canvas, useThree, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+const IdealismComponent = dynamic(() => import("components/idealism"), { ssr: false });
 
-const normalDistribution = (x) => Math.exp((-x * x) / 2) / Math.sqrt(2 * Math.PI);
-
-export default function Test() {
-  const VERTICAL_NUMBERS = 11;
-  const HORIZONTAL_NUMBERS = 51;
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas camera={{ position: [10, 10, 10] }}>
-        <ambientLight intensity={1} />
-
-        <group>
-          {new Array(VERTICAL_NUMBERS).fill(0).map((_, height) =>
-            new Array(HORIZONTAL_NUMBERS).fill(0).map((_, i) => (
-              <mesh position={[i - (HORIZONTAL_NUMBERS - 1) / 2, 0, normalDistribution((i - (HORIZONTAL_NUMBERS - 1) / 2) / 10) * 5 * (height - (VERTICAL_NUMBERS - 1) / 2)]} key={i}>
-                <sphereGeometry attach="geometry" args={[1, 32, 32]} />
-                <meshStandardMaterial attach="material" color="rgb(0,0,0)" />
-              </mesh>
-            ))
-          )}
-        </group>
-        <OrbitControls />
-        {/* <DownloadThreeScene /> */}
-      </Canvas>
-    </div>
-  );
+export default function Idealism() {
+  return <IdealismComponent />;
 }
