212bda3e2833f08af1e2f31edcf68d2ff9906c6d
ericggul <57379903+ericggul@users.noreply.github.com>
2022-11-04 13:14:29 +0000

    Feat: W Colour

--- components/w/colour/index.js
+++ components/w/colour/index.js
@@ -2,8 +2,17 @@ import * as S from "./styles";
 import useResize from "utils/hooks/useResize";
 import { useState, useEffect, useMemo } from "react";
 import { useRouter } from "next/router";
+import * as Tone from "tone";
 
 const getRandom = (a, b) => Math.random() * (b - a) + a;
+
+const CODES = ["C", "D", "E", "F", "G", "A", "B"];
+function randomnessStepToCodeConverter(randomnessState) {
+  let floored = Math.floor(randomnessState);
+  console.log(floored);
+  console.log(`${CODES[floored % 7]}${Math.floor(floored / 7)}`);
+  return `${CODES[floored % 7]}${Math.floor(floored / 7)}`;
+}
 
 function Colour() {
   const [windowWidth, windowHeight] = useResize();
@@ -30,6 +39,8 @@ function Colour() {
 
   useEffect(() => {
     const handleKeyDown = (e) => {
+      Tone.start();
+
       if (e.code === "KeyW") {
         setRandomnessState((s) => s + randomnessStep);
       }
@@ -46,20 +57,20 @@ function Colour() {
   }, [randomnessStep]);
 
   useEffect(() => {
-    if (randomnessState >= 50) {
+    if (randomnessState >= 60) {
       setRandomnessState(0);
       setCycleState((s) => s + 1);
       if (cycleState < 15) {
-        setRandomnessStep((s) => s * 1.22);
+        setRandomnessStep((s) => s * 1.2);
       } else {
-        setRandomnessStep((s) => s * 1.025);
+        setRandomnessStep((s) => s * 1.035);
       }
     }
   }, [randomnessState, randomnessStep]);
 
   const router = useRouter();
   useEffect(() => {
-    if (randomnessStep > 50) {
+    if (randomnessStep > 55) {
       router.push("/w/tunnel");
     }
   }, [randomnessStep]);
@@ -74,6 +85,19 @@ function Colour() {
 }
 
 const Inner = ({ i, elements, randomnessState }) => {
+  const synth = useMemo(() => new Tone.PolySynth().toDestination(), []);
+
+  useEffect(() => {
+    if (i === 0) {
+      try {
+        const now = Tone.now();
+        synth.triggerAttackRelease(randomnessStepToCodeConverter(randomnessState), "32n", now);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, [i, randomnessState]);
+
   const [windowWidth, windowHeight] = useResize();
 
   const widthRandomness = useMemo(() => {
@@ -82,7 +106,7 @@ const Inner = ({ i, elements, randomnessState }) => {
       const max = 0.92 + (0.0001 + randomnessState * 0.000015) * randomnessState;
       return getRandom(min, max) ** i;
     } else {
-      const min = Math.max(0.92 - 0.002 * Math.pow(randomnessState - 35, 2.2), -2);
+      const min = Math.max(0.92 - 0.002 * Math.pow(randomnessState - 35, 2), -2);
       const max = Math.min(0.99, 0.92 + 0.0005 * (randomnessState + 6));
       return getRandom(min, max) ** i;
     }
