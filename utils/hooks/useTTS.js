13d5539d3c754c3613e279a69428bc8bb761f6f2
ericggul <57379903+ericggul@users.noreply.github.com>
2022-12-23 23:11:43 +0000

    Feat: useTTS Hook, TriggerMusic, Now deleting from 17square

---
+++ utils/hooks/useTTS.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function useTTS(text, speak, setSpeak) {
+  const [arrayBufferData, setArrayBufferData] = useState(null);
+
+  useEffect(() => {
+    getTTS(text);
+  }, [text]);
+
+  useEffect(() => {
+    if (speak && arrayBufferData) {
+      playAudio(arrayBufferData);
+      setSpeak(false);
+    }
+  }, [speak, arrayBufferData]);
+
+  async function getTTS(text) {
+    try {
+      const res = await axios.post(
+        "/api/google-tts",
+        { text },
+        {
+          responseType: "arraybuffer",
+        }
+      );
+      let data = res.data;
+
+      setArrayBufferData(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+async function playAudio(data) {
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const audioCtx = new AudioContext();
+  const audioBuffer = await audioCtx.decodeAudioData(data);
+  const source = audioCtx.createBufferSource();
+  source.buffer = audioBuffer;
+  source.connect(audioCtx.destination);
+  source.start();
+}
