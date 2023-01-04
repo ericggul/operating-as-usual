41cce0dd03b6fa722dc65aef1f3727aac4e43a97
ericggul <57379903+ericggul@users.noreply.github.com>
2023-01-04 13:55:41 +0000

    Fix: Deploy Bug Fix

--- pages/experiments/model-viewer.js
+++ pages/experiments/model-viewer.js
@@ -1,8 +1,15 @@
 import dynamic from "next/dynamic";
+import Script from "next/script";
+
 const Component = dynamic(() => import("components/experiments/ModelViewer"), {
   ssr: false,
 });
 
 export default function SafariAR() {
-  return <Component />;
+  return (
+    <>
+      <Script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js" />
+      <Component />
+    </>
+  );
 }
