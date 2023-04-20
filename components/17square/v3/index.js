46ce37d9f9559dd3258efca0ddeb8bb1c4bb5f4f
ericggul <57379903+ericggul@users.noreply.github.com>
2023-04-20 13:45:05 +0100

    Feat

--- components/17square/v3/index.js
+++ components/17square/v3/index.js
@@ -20,7 +20,7 @@ export default function Container() {
 
   const audioRef = useRef();
 
-  const [speak, setSpeak] = useState(false);
+  const [speak, setSpeak] = useState(true);
   useTTS(TEXT, speak, setSpeak);
 
   useEffect(() => {
