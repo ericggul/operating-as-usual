185c7a37cf03ed3ea8b3072213a5e081734eba4b
ericggul <57379903+ericggul@users.noreply.github.com>
2023-12-26 16:38:53 +0000

    Feat: iphone sounds

--- pages/w/components/tunnel.js
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const TunnelComponent = dynamic(() => import("components/w/tunnel"), { ssr: false });
-
-export default function Tunnel() {
-  return <TunnelComponent />;
-}
--- pages/w/phase2.js
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const ChatComponent = dynamic(() => import("components/w/chat"), { ssr: false });
-
-export default function Tunnel() {
-  return <ChatComponent />;
-}
